b415d7bd9de7fc7dd134393823453240f90f991b
alphacoder01 <ashishgoswami8956230@gmail.com>
2019-09-27 02:00:59 +0530

    added  comments for readability

--- Stacks/prefix_to_infix.py
+++ Stacks/prefix_to_infix.py
@@ -1,20 +1,20 @@
-from stack_class import Stack
-s = Stack()
+from stack_class import Stack           #importing the stack class
+s = Stack()                             #Stack object
 
-infix = ''
+infix = ''                                      
 prefix = input("Enter the prefix string ")
-prefix_reversed = reversed(prefix)
+prefix_reversed = reversed(prefix)      #reversed prefix srting
 
 for i in prefix_reversed:
-    if i.isalpha():
+    if i.isalpha():                     #if i is operand then push onto stack
         s.push(i)
     else:
-        op1 = s.peek()
+        op1 = s.peek()                 #else pop 2 operands from stack and form a string as shown 
         s.pop()
         op2 = s.peek()
         s.pop()
         infix = op1 + i + op2
         infix = '('+infix+')'
-        s.push(infix) 
+        s.push(infix)                   #finally push the string onto the stack.
 print(infix)
 
